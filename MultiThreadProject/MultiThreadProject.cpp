88c5ed038227b10b93ee4e9356f392c0cd56db60
maxefimov <efimov.academy.top@gmail.com>
2023-02-06 16:27:04 +0300

    Добавьте файлы проекта.

---
+++ MultiThreadProject/MultiThreadProject.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+int cnt{};
+
+mutex m;
+
+void func()
+{
+    for (int i = 0; i < 100; i++)
+        cout << this_thread::get_id() << " " << i << "\n";
+}
+
+void counter()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        lock_guard<mutex> lg(m);
+        cout << this_thread::get_id() << " " << cnt << "\n";
+        cnt++;   
+    }
+}
+
+int main()
+{
+    thread th1(counter);
+    thread th2(counter);
+
+
+    th1.join();
+    th2.join();
+
+    cout << cnt;
+}
